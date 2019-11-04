7f5f481dcb9c0390e03c1b3e77f84f6ca15ec31c
Tp1SORO <56545247+Tp1SORO@users.noreply.github.com>
2019-11-04 14:50:01 +0100

    rer

---
+++ tp2/formulaire.js
@@ -0,0 +1,23 @@
+
+<html>
+
+<head>
+    <title>Validation Javascript</title>
+<script type='text/javascript' src='formulaire.js'>
+    function Validation(){
+        var date=document.getElementById('birthday').value//selection d un seul elmt  
+        //element html
+        //ou
+        //document.querySelector('#birthday').value
+        //(selecteur css)  
+        //selection d'un seul element
+        //var list=document.querrySelectorAll('input'); 
+        //list.forEach(function(InputItem){ une boucle foreach
+        //if(inputItem.value.length<5){
+        //error+='champ vide';   
+<input type="button" value="Valider" onclick="validation()"></input>
+    }
+
+</script>
+</head>
+</html>
