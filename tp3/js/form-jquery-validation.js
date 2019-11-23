49543d6d04270c74a2c56c849dab46c6894079f8
Tp1SORO <56545247+Tp1SORO@users.noreply.github.com>
2019-11-23 04:07:55 +0100

    cr22
    cr222

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,24 +1,47 @@
 $(document).ready(function () {
-
+   
     // interception du "submit" du formulaire
     $("form").on("submit", function (event) {
         event.preventDefault();
-         var error = "";
+        var error = "";
         // votre code de vérification du formulaire
-        if($('#name').val().length < 5){
-           error += "nom, ";
+        if ($("#name").val().length < 5) {
+            error += "nom, ";
         }
-        if($('#firstname').val().length < 5){
-           error += "prénom, ";
+        if ($("#firstname").val().length < 5) {
+            error += "prénom, ";
         }
+        if ($("#adresse").val().length < 5) {
+            error += "adresse, ";
+    }
+   
 
-        if(error == ""){
-            $(".modal-body").html('Bienvenue ' + $('#name').val());
-        }else{
-            $(".modal-body").html(error + ' obligatoire');
+
+        if (error == "") {
+            $(".modal-body").html("Bienvenue " + $("#name").val());
+        } else {
+            $(".modal-body").html(error + " obligatoire");
         }
+        if (error == "") {
+            $(".modal-body").html("Bienvenue " + $("#firstname").val());
+        } else {
+            $(".modal-body").html(error + " obligatoire");
+        
+        }
+        if (error == "") {
+            $(".modal-body").html("Bienvenue " + $("#adresse").val());
+        } else {
+            $(".modal-body").html(error + " obligatoire");
+        
+        }
+       
+     
+    
+
 
         $('#myModal').modal("show");
+
+     
 
     });
 });
